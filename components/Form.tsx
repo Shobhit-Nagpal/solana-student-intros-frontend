84b9fb875829aa90603b6c18e19d1206a8dd4e19
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-03-23 18:01:22 +0530

    Add student intro to devnet

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,58 +1,114 @@
-import { FC } from 'react'
-import { StudentIntro } from '../models/StudentIntro'
-import { useState } from 'react'
-import { Box, Button, FormControl, FormLabel, Input, Textarea } from '@chakra-ui/react'
+import { FC } from "react";
+import { StudentIntro } from "../models/StudentIntro";
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import * as web3 from "@solana/web3.js";
 
-const STUDENT_INTRO_PROGRAM_ID = 'HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf'
+const STUDENT_INTRO_PROGRAM_ID = "HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf";
 
 export const Form: FC = () => {
-    const [name, setName] = useState('')
-    const [message, setMessage] = useState('')
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
 
-    const handleSubmit = (event: any) => {
-        event.preventDefault()
-        const studentIntro = new StudentIntro(name, message)
-        handleTransactionSubmit(studentIntro)
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const studentIntro = new StudentIntro(name, message);
+    handleTransactionSubmit(studentIntro);
+  };
+
+  const handleTransactionSubmit = async (studentIntro: StudentIntro) => {
+    if (!publicKey) {
+      alert("Please connect your wallet!");
+      return;
     }
 
-    const handleTransactionSubmit = async (studentIntro: StudentIntro) => {
-        console.log(JSON.stringify(studentIntro))
-    }
+    const buffer = studentIntro.serialize();
+    const txn = new web3.Transaction();
 
-    return (
-        <Box
-            p={4}
-            display={{ md: "flex" }}
-            maxWidth="32rem"
-            borderWidth={1}
-            margin={2}
-            justifyContent="center"
-        >
-            <form onSubmit={handleSubmit}>
-                <FormControl isRequired>
-                    <FormLabel color='gray.200'>
-                        What do we call you?
-                    </FormLabel>
-                    <Input 
-                    id='name' 
-                    color='gray.400'
-                    onChange={event => setName(event.currentTarget.value)}
-                />
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel color='gray.200'>
-                        What brings you to Solana, friend?
-                    </FormLabel>
-                    <Textarea 
-                        id='message' 
-                        color='gray.400'
-                        onChange={event => setMessage(event.currentTarget.value)}
-                    />
-                </FormControl>
-                <Button width="full" mt={4} type="submit">
-                    Submit
-                </Button>
-            </form>
-        </Box>
+    const [pda] = await web3.PublicKey.findProgramAddress(
+      [publicKey.toBuffer()],
+      new web3.PublicKey(STUDENT_INTRO_PROGRAM_ID),
     );
-}
+
+    const instruction = new web3.TransactionInstruction({
+      keys: [
+        {
+          pubkey: publicKey,
+          isSigner: true,
+          isWritable: false,
+        },
+        {
+          pubkey: pda,
+          isSigner: false,
+          isWritable: true,
+        },
+        {
+          pubkey: web3.SystemProgram.programId,
+          isSigner: false,
+          isWritable: false,
+        },
+      ],
+      programId: new web3.PublicKey(STUDENT_INTRO_PROGRAM_ID),
+      data: buffer,
+    });
+
+    txn.add(instruction);
+
+    try {
+      const txnSig = await sendTransaction(txn, connection);
+      console.log(
+        `Transaction submitted: https://explorer.solana.com/tx/${txnSig}?cluster=devnet`,
+      );
+    } catch (err) {
+      console.log(JSON.stringify(err));
+      alert(JSON.stringify(err));
+    }
+    console.log(JSON.stringify(studentIntro));
+  };
+
+  return (
+    <Box
+      p={4}
+      display={{ md: "flex" }}
+      maxWidth="32rem"
+      borderWidth={1}
+      margin={2}
+      justifyContent="center"
+    >
+      <form onSubmit={handleSubmit}>
+        <FormControl isRequired>
+          <FormLabel color="gray.200">What do we call you?</FormLabel>
+          <Input
+            id="name"
+            color="gray.400"
+            onChange={(event) => setName(event.currentTarget.value)}
+          />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel color="gray.200">
+            What brings you to Solana, friend?
+          </FormLabel>
+          <Textarea
+            id="message"
+            color="gray.400"
+            onChange={(event) => setMessage(event.currentTarget.value)}
+          />
+        </FormControl>
+        <Button width="full" mt={4} type="submit">
+          Submit
+        </Button>
+      </form>
+    </Box>
+  );
+};
